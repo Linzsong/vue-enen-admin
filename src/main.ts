fcb00fe7f9553272836b9abfdaa3fc543a21cc9c
Linzisong <744740666@qq.com>
2024-07-27 00:52:28 +0800

    feat: 项目初始化

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,17 @@
 import { createApp } from 'vue'
+// import './styles/tailwind.css';
+// import './styles/index.less';
 import './style.css'
-import App from './App.vue'
+// import { setupNaiveDiscreteApi, setupNaive, setupDirectives } from '@/plugins';
+import App from './App.vue';
+// import router, { setupRouter } from './router';
+import { pinia } from '@/store';
+console.log(pinia);
 
-createApp(App).mount('#app')
+(async() => {
+  const app = createApp(App)
+
+  app.use(pinia)
+  
+  app.mount('#app')
+})()
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
+console.log(resolve('./src/'));
+console.log(resolve(process.cwd(), '.', 'src') + '/');
+
+
 export default defineConfig({
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        // resolve(process.cwd(), '.', dir);
+        replacement: resolve(process.cwd(), '.', 'src') + '/',
+      },
+    ],
+    // alias: {
+    //   '@': './src'
+    // },
+    dedupe: ['vue'],
+  },
   plugins: [vue()],
 })
